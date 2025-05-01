49198ae037d6170f4df51a4e03ce386ddcb3c90a
Mateus-Redivo <80864178+Mateus-Redivo@users.noreply.github.com>
2025-05-01 12:42:19 -0300

    Inserindo os codigos da API

---
+++ API REST/Populacao.sql
@@ -0,0 +1,26 @@
+-- Inserir Produtos (sem categoria)
+INSERT INTO produto (nome, descricao, preco, quantidade) VALUES 
+-- Eletrônicos
+('Smart TV 55"', 'Smart TV LED 4K 55 polegadas', 3299.90, 15),
+('Soundbar 2.1', 'Soundbar com subwoofer wireless', 899.90, 20),
+('Ar Condicionado Split', 'Ar Condicionado Split 12000 BTUs', 2199.90, 10),
+
+-- Informática
+('Notebook Gamer', 'Notebook com processador i7 e placa dedicada', 5999.90, 8),
+('Mouse Gamer', 'Mouse RGB 16000 DPI', 299.90, 30),
+('SSD 1TB', 'SSD NVME 1TB', 599.90, 25),
+
+-- Smartphones
+('iPhone 15', 'iPhone 15 128GB', 5999.90, 12),
+('Galaxy S23', 'Samsung Galaxy S23 256GB', 4999.90, 15),
+('Carregador Wireless', 'Carregador wireless 15W', 199.90, 40),
+
+-- Games
+('PlayStation 5', 'Console PlayStation 5 Digital', 3999.90, 10),
+('Xbox Series X', 'Console Xbox Series X', 4299.90, 8),
+('Nintendo Switch', 'Console Nintendo Switch OLED', 2499.90, 12),
+
+-- Audio
+('Fone Bluetooth', 'Fone de ouvido bluetooth com cancelamento de ruído', 899.90, 25),
+('Caixa de Som Portátil', 'Caixa de som bluetooth à prova d''água', 399.90, 30),
+('Microfone Condensador', 'Microfone condensador USB', 599.90, 18);
